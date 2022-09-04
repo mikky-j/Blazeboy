4dfd121cf5f182db84151f13d49f13ec3bffa0f0
mikky-j <moregunwa@gmail.com>
2022-09-05 00:30:15 +0100

    added oam and vram

--- src/memory.rs
+++ src/memory.rs
@@ -1,7 +1,9 @@
+use std::{fs::File, io::BufReader};
+
 use crate::{
     cartridge::{Cartridge, CartridgeError},
-    Bus,
-    io_registers::IORegisters, Wrapper
+    io_registers::IORegisters,
+    Bus, Wrapper,
 };
 
 #[derive(Debug)]
@@ -26,36 +28,57 @@ impl MemoryError {
 }
 pub struct Memory {
     wram: [u8; 0x2000],
-    hram: [u8; 0x7e],
+    hram: [u8; 0x7f],
+    vram: [u8; 0x2000],
+    oam: [u8; 0xa0],
+    boot_rom: BufReader<File>,
+    booting: bool,
     cartridge: Cartridge,
     io_registers: Wrapper<IORegisters>,
 }
 
 impl Memory {
-    pub fn new(cartridge: Cartridge, io_registers: Wrapper<IORegisters>) -> Self {
+    pub fn new(
+        cartridge: Cartridge,
+        boot_rom: BufReader<File>,
+        io_registers: Wrapper<IORegisters>,
+    ) -> Self {
         Memory {
             wram: [0; 0x2000],
-            hram: [0; 0x7e],
+            vram: [0; 0x2000],
+            hram: [0; 0x7f],
+            oam:[0; 0xa0],
             cartridge,
-            io_registers
+            boot_rom,
+            booting: true,
+            io_registers,
         }
     }
 }
 
 impl Bus for Memory {
     fn read(&mut self, address: u16) -> Result<u8, MemoryError> {
-        if address < 0x8000 {
+        if address < 0x100 && self.booting {
+            let vector = crate::cartridge::read(&mut self.boot_rom, address as usize, 1)
+                .map_err(|v| MemoryError::CartridgeError(v))?;
+            return Ok(vector[0]);
+        } else if address < 0x8000 {
+            // self.booting = false;
             //? ROM DATA
-            return self.cartridge
+            return self
+                .cartridge
                 .cart_read(address)
                 .map_err(|v| MemoryError::CartridgeError(v));
         } else if address < 0xA000 {
             //? VIDEO RAM
-            println!("Unsupported Reading from {:04x}", address)
+            let offset =  address - 0x8000;
+            // println!("Unsupported Reading from {:04x}", address)
+            return Ok(self.vram[offset as usize])
             // unimplemented!("Tried to access `{address:02x}`")
         } else if address < 0xC000 {
             //? CARTRIDGE RAM
-            return self.cartridge
+            return self
+                .cartridge
                 .cart_read(address)
                 .map_err(|v| MemoryError::CartridgeError(v));
         } else if address < 0xE000 {
@@ -66,7 +89,7 @@ impl Bus for Memory {
             } else {
                 return Err(MemoryError::InvalidRead(address + 0xC000));
             }
-        } else if address < 0xFE00{
+        } else if address < 0xFE00 {
             //? ECHO RAM
             let address = address - 0xE000;
             if address < 0x2000 {
@@ -74,9 +97,11 @@ impl Bus for Memory {
             } else {
                 return Err(MemoryError::InvalidRead(address + 0xC000));
             }
-        }else if address < 0xFEA0 {
+        } else if address < 0xFEA0 {
             //? OAM DATA
-            println!("Unsupported Reading from {:04x}", address)
+            let offset =  address - 0xFE00;
+            return Ok(self.oam[offset as usize])
+            // println!("Unsupported Reading from {:04x}", address)
             // unimplemented!("Tried to access `{address:02x}`")
         } else if address < 0xFF00 {
             //? NOT USABLE AT ALL
@@ -86,10 +111,10 @@ impl Bus for Memory {
             //? IO REGISTERS
             return self.io_registers.borrow_mut().read(address);
             // unimplemented!("Tried to access `{address:02x}`")
-        } else if address < 0xFFFE {
+        } else if address < 0xFFFF {
             //? HI RAM
             let address = address - 0xFF80;
-            if address < 0x7e {
+            if address < 0x7f {
                 return Ok(self.hram[address as usize]);
             } else {
                 return Err(MemoryError::InvalidRead(address + 0xFF80));
@@ -106,14 +131,15 @@ impl Bus for Memory {
         if address < 0x8000 {
             //? ROM DATA
             self.cartridge.cart_write(address, value);
-        }else if address < 0xA000 {
+        } else if address < 0xA000 {
             //? VIDEO RAM
-            println!("Unsupported Writing to {:04x}", address)
+            let offset =  address - 0x8000;
+            self.vram[offset as usize] = value;
+            // println!("Unsupported Writing to {:04x}", address)
             // unimplemented!("Tried to access `{address:02x}`")
         } else if address < 0xC000 {
             //? CARTRIDGE RAM
-            self.cartridge
-                .cart_write(address, value);
+            self.cartridge.cart_write(address, value);
         } else if address < 0xE000 {
             //? WORK RAM
             let address = address - 0xC000;
@@ -125,9 +151,11 @@ impl Bus for Memory {
         } else if address < 0xFE00 {
             //? ECHO RAM
             return Err(MemoryError::InvalidWrite(address));
-        }else if address < 0xFEA0 {
+        } else if address < 0xFEA0 {
             //? OAM DATA
-            println!("Unsupported Writing to {:04x}", address)
+            let offset =  address - 0xFE00;
+            self.oam[offset as usize] = value;
+            // println!("Unsupported Writing to {:04x}", address)
             // unimplemented!("Tried to access `{address:02x}`")
         } else if address < 0xFF00 {
             //? NOT USABLE AT ALL
@@ -136,10 +164,10 @@ impl Bus for Memory {
             //? IO REGISTERS
             return self.io_registers.borrow_mut().write(address, value);
             // unimplemented!("Tried to access `{address:02x}`")
-        } else if address < 0xFFFE {
+        } else if address < 0xFFFF {
             //? HI RAM
             let address = address - 0xFF80;
-            if address < 0x7e {
+            if address < 0x7f {
                 self.hram[address as usize] = value;
             } else {
                 return Err(MemoryError::InvalidWrite(address + 0xFF80));
