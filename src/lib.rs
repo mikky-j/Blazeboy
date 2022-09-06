2fba988bc0b4847a1f50d41dcfbf9772d7907f73
mikky-j <moregunwa@gmail.com>
2022-09-06 23:02:18 +0100

    added some ppu stuff

--- src/lib.rs
+++ src/lib.rs
@@ -6,15 +6,19 @@ mod instruction_data;
 mod io_registers;
 mod memory;
 mod ppu;
+mod ppu_registers;
 
 use std::{cell::RefCell, fmt::Display, fs::File, io::BufReader, rc::Rc};
 
 use cartridge::{Cartridge, CartridgeError};
 use cpu::Cpu;
 use memory::{Memory, MemoryError};
+use ppu::Ppu;
+use ppu_registers::PPURegisters;
 
 /// This is a wrapper type that enables other modules to store a reference to the type T
 pub type Wrapper<T> = Rc<RefCell<T>>;
+
 
 pub enum EmulatorError {
     CatridgeError(CartridgeError),
@@ -32,12 +36,14 @@ impl Display for EmulatorError {
 
 pub struct Emulator {
     cpu: Cpu<Memory>,
+    ppu: Ppu
 }
 
 impl Emulator {
     pub fn new(cart_filename: &str) -> Result<Self, EmulatorError> {
         let cartridge =
             Cartridge::new(cart_filename).map_err(|v| EmulatorError::CatridgeError(v))?;
+        let ppu_registers = Rc::new(RefCell::new(PPURegisters::new()));
         let io_registers = Rc::new(RefCell::new(io_registers::IORegisters::default()));
         let boot_file = File::open("src/roms/boot.gb").map_err(|_| {
             EmulatorError::CatridgeError(CartridgeError::FileDoesNotExist(format!(
@@ -45,10 +51,11 @@ impl Emulator {
             )))
         })?;
         let boot_rom = BufReader::with_capacity(256, boot_file);
-        let memory = Memory::new(cartridge, boot_rom, io_registers);
+        let memory = Memory::new(cartridge, boot_rom, io_registers, ppu_registers.clone());
         let mem_ref = Rc::new(RefCell::new(memory));
-        let cpu = Cpu::new(&mem_ref);
-        Ok(Emulator { cpu })
+        let cpu = Cpu::new(mem_ref.clone());
+        let ppu = Ppu::new(ppu_registers.clone());
+        Ok(Emulator { cpu, ppu })
     }
 
     pub fn run(&mut self) -> Result<(), EmulatorError> {
